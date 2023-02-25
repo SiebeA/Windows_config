da70845415c463e6b5743a394ddb51b2b2b08cee
Siebe Albers <savdelz@gmail.com>
2023-02-25 20:05:11 +0100

    provide context + unique matches only

--- siebe_commandLine_Python_utilities/findall.py
+++ siebe_commandLine_Python_utilities/findall.py
@@ -15,14 +15,23 @@ def main(pattern):
         pattern = pattern
         regex = re.compile(pattern, re.MULTILINE)
         matches = regex.findall(text)
+        # only print unique matches
+        matches = list(set(matches))
 
         # export to a text file
         with open('temp_findall.txt', 'w', encoding="utf-8") as f:
             for match in matches:
+                start_index = text.find(match)
+                end_index = start_index + len(match)
+                context_start = max(start_index - 40, 0)
+                context_end = min(end_index + 40, len(text))
+                context = text[context_start:context_end]
                 print()
-                print(match)
-                f.write(match + '\n')
-            print("\n A text file with the results has been created in the same folder.\n")
+                print("Match: ", match)
+                print("Context: ", context)
+                f.write("Match: " + match + '\n')
+                f.write("Context: " + context + '\n\n')
+            print("\nA text file with the results has been created in the same folder.\n")
 
 if __name__ == '__main__':
     pattern = input("Enter the regex pattern to match: ")
