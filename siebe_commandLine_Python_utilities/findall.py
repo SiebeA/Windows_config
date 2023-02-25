e57be2b7650f0dfeec5bc7da2286af3f8e8864ab
Siebe Albers <savdelz@gmail.com>
2023-02-25 19:48:04 +0100

    allow for dynamic file path input, for dynamic use regardless of folder

--- siebe_commandLine_Python_utilities/findall.py
+++ siebe_commandLine_Python_utilities/findall.py
@@ -1,11 +1,12 @@
-import argparse
 import re
 
-def main(pattern, input):
+def main(pattern):
     """
-    (command line able) Find all matches for a given regex pattern in a text file
+    Find all matches for a given regex pattern in a text file
     """
-    with open(input, 'r',encoding="utf-8") as f:
+    input_file = input("Enter the path to the input text file: ")
+    
+    with open(input_file, 'r', encoding="utf-8") as f:
         text = f.read()
         # enable global and multiline matching
         pattern = pattern
@@ -18,18 +19,8 @@ def main(pattern, input):
                 print()
                 print(match)
                 f.write(match + '\n')
-            print("\n a text file with the results has been created in the same folder.\n")
+            print("\n A text file with the results has been created in the same folder.\n")
 
 if __name__ == '__main__':
-    # Create an ArgumentParser object
-    parser = argparse.ArgumentParser()
-
-    # Add arguments to the parser
-    parser.add_argument('--input', required=True, help='The path to the text file')
-    parser.add_argument('--pattern', required=True, help='The regex pattern to match')
-
-    # Parse the command-line arguments
-    args = parser.parse_args()
-
-    # Call the main function with the parsed arguments
-    main(input=args.input, pattern=args.pattern)
+    pattern = input("Enter the regex pattern to match: ")
+    main(pattern)
