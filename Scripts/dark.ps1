e2945b1fa4053664748f81599d3b6d1226c34ca8
Siebe Albers <savdelz@gmail.com>
2023-05-19 08:02:47 +0300

    FIX now the apps also adapt, not just taskbar etc

--- Scripts/dark.ps1
+++ Scripts/dark.ps1
@@ -1,6 +1,9 @@
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-$regName = "SystemUsesLightTheme"
+$regName1 = "SystemUsesLightTheme"
+$regName2 = "AppsUseLightTheme"
 $regValue = 0
-Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
+
+Set-ItemProperty -Path $regPath -Name $regName1 -Value $regValue
+Set-ItemProperty -Path $regPath -Name $regName2 -Value $regValue
 
 Get-Process explorer | Stop-Process -Force
--- Scripts/light.ps1
+++ Scripts/light.ps1
@@ -1,6 +1,11 @@
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-$regName = "SystemUsesLightTheme"
-$regValue = 1
-Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
+$regName1 = "SystemUsesLightTheme"
+$regName2 = "AppsUseLightTheme"
+$regName3 = "AppsUseDarkTheme"
+$regValue1 = 1
+$regValue2 = 0
+Set-ItemProperty -Path $regPath -Name $regName1 -Value $regValue1
+Set-ItemProperty -Path $regPath -Name $regName2 -Value $regValue1
+Set-ItemProperty -Path $regPath -Name $regName3 -Value $regValue2
 
 Get-Process explorer | Stop-Process -Force
