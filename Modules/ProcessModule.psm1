145a8a30dc3abfa4be07c29e48cb0fd59a5ddc99
Siebe Albers <savdelz@gmail.com>
2023-07-23 19:54:38 +0200

    FEAT only output last 20 lines

--- Modules/ProcessModule.psm1
+++ Modules/ProcessModule.psm1
@@ -8,33 +8,25 @@ function tSearch { # aka "text-file search function"
         Get-Content $FilePath | Select-String $SearchPattern
     }
 
-function hSearch {
-    <#
-        aka "history search function"
-Synopsis
-    This function searches the history.txt file for a given string
-
-    #>
-    param(
-        [string]$SearchPattern
-    )
-
-    Get-Content "C:\Users\Siebe\Documents\PowerShell\history.txt" | Select-String $SearchPattern
-}
-
-# function htaill 
-function htail {
-    param(
-        [Parameter(Mandatory=$false, Position=1)]
-        [string]$FilePath = "C:\Users\Siebe\Documents\PowerShell\history.txt",
-        
-        [Parameter(Mandatory=$false, Position=0)]
-        [int]$Lines = 10
-    )
-
-    Get-Content -Tail $Lines $FilePath
-
-}
+    function hSearch {
+        <#
+            aka "history search function"
+        Synopsis:
+            This function searches the history.txt file for a given string
+        #>
+        param(
+            [string]$SearchPattern
+        )
+    
+        $matches = Get-Content "C:\Users\Siebe\Documents\PowerShell\history.txt" | Select-String $SearchPattern
+    
+        # Check if there are more than 20 matches, then output only the last 20 matches
+        if ($matches.Count -gt 20) { # gt = greater than
+            $matches | Select-Object -Last 20
+        } else {
+            $matches
+        }
+    }
 
 function stopProcess {
     param(
